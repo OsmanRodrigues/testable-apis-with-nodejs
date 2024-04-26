8f5ac4b5f7e8b43c12111adb22d8ea87473a361a
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-25 23:24:36 -0300

    tdd(green): products controller get method impl

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,9 +1,23 @@
 import ProductsController from '../../../src/controllers/products';
+import sinon from 'sinon';
 
 describe('Controllers: Products', () => {
+    const defaultProduct = [{
+        name: 'Default product',
+        description: 'product description',
+        price: 100
+    }]
     describe('get() products', () => {
         it('should return a list of products', () => {
+            const req = {};
+            const res = {
+                send: sinon.spy()
+            };
             const productsController = new ProductsController();
+            productsController.get(req, res);
+
+            expect(res.send.called).to.be.true;
+            expect(res.send.calledWith(defaultProduct)).to.be.true;
         });
     });
 });
