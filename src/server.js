69bb23bcb3e183fee65fceddba83b87c26426a99
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-27 13:50:52 -0300

    server: async server execution and graceful shutdown refactor

--- src/server.js
+++ src/server.js
@@ -1,6 +1,32 @@
-import app from "./app"
+import setupApp from "./app";
+
 const port = 8080;
 
-app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-})
+(async () => {
+    try {
+        const app = await setupApp();
+        const server = app.listen(port, () => {
+            console.log(`app running on port ${port}`);
+        });
+        const exitSignals = [
+            'SIGINT',
+            'SIGTERM',
+            'SIGQUIT'
+        ];
+        exitSignals.map(sign => process.on(
+            sign,
+            () => server.close(err => {
+                if (err) {
+                    console.error(err);
+                    process.exit(1);
+                };
+                app.database.connection.close(function () {
+                    console.info('Database connection closed!');
+                    process.exit(0);
+                });
+            })))
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+})();
