0ab3df505f7b60b90abb3be80958940421e3ade7
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-27 17:28:20 -0300

    tdd(refactor): products controller model injection and integration impl

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,11 @@
 class ProductsController {
-    get(req, res) {
-        return res.send([{
-            name: 'Default product',
-            description: 'product description',
-            price: 100
-        }])
+    constructor(Product) { 
+        this.Product = Product;
+    }
+    
+    async get(req, res) {
+        const products = await this.Product.find({});
+        return res.send(products);
     }
  };
 
