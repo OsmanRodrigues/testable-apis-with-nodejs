9fccfe8297c897513e0e40c0365c08d6cbfa5edf
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-27 17:17:27 -0300

    tdd(red): products controller unit tests models link impl

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,5 +1,6 @@
 import ProductsController from '../../../src/controllers/products';
 import sinon from 'sinon';
+import Product from '../../../src/models/product';
 
 describe('Controllers: Products', () => {
     const defaultProduct = [{
@@ -8,16 +9,18 @@ describe('Controllers: Products', () => {
         price: 100
     }]
     describe('get() products', () => {
-        it('should return a list of products', () => {
+        it('should return a list of products', async () => {
             const req = {};
             const res = {
                 send: sinon.spy()
             };
-            const productsController = new ProductsController();
-            productsController.get(req, res);
-
-            expect(res.send.called).to.be.true;
-            expect(res.send.calledWith(defaultProduct)).to.be.true;
+            Product.find = sinon.stub;
+            Product.find.withArgs({}).resolves(defaultProduct);
+            const productsController = new ProductsController(Product);
+            
+            await productsController.get(req, res);
+            
+            sinon.assert.calledWith(res.send, defaultProduct);
         });
     });
 });
