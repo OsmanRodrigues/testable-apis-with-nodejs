6e839b31eaa147cdbfaad0ac1e483dea16829cdf
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-29 11:52:05 -0300

    tdd(refactor): products controller get by id model integration impl

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -13,14 +13,14 @@ class ProductsController {
     }
 
     async getById(req, res) {
-        const response = await Promise.resolve([{
-            __v: 0,
-            _id: '56cb91bdc3464f14678934ca',
-            name: 'Default product',
-            description: 'product description',
-            price: 100
-        }]);
-        res.send(response);
+        const { params: { id } } = req;
+        
+        try {
+            const product = await this.Product.find({ _id: id });
+            res.send(product);
+        } catch (err) {
+            res.status(400).send(err.message);
+        }
     }
  };
 
