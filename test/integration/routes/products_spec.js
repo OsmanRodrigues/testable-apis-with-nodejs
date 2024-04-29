af381a7a7e8cf080dabd7eda55151e5f658b9f60
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-04-29 17:01:27 -0300

    refactor: update product http method changed to put

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -76,23 +76,26 @@ describe('Routes: Products', () => {
                     });
             });
         });
-        context('when update a product', () => {
-            it('should return the updated product with status code 200', done => {
-                const id = expectedProduct._id;
-                const updatedProduct = {
-                  ...expectedProduct,
-                  name: 'Updated product',
-                };
+    });
 
-                request
-                    .post(`/products/update/${id}`)
-                    .send(updatedProduct)
-                    .end((err, res) => { 
-                        expect(res.statusCode).to.eql(200);
-                        expect(res.body).to.eql(updatedProduct);
-                        done(err);
-                    });
-            });
+    describe('PUT /products/:id', () => {
+        context('when update a product', () => {
+          it('should return the updated product with status code 200', (done) => {
+            const id = expectedProduct._id;
+            const updatedProduct = {
+              ...expectedProduct,
+              name: 'Updated product',
+            };
+
+            request
+              .put(`/products/${id}`)
+              .send(updatedProduct)
+              .end((err, res) => {
+                expect(res.statusCode).to.eql(200);
+                expect(res.body).to.eql(updatedProduct);
+                done(err);
+              });
+          });
         });
     });
 });
