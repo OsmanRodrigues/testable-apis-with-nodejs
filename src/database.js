a662aa63ea33009a476d90951ee84ff3fed845f3
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-06 21:58:17 -0300

    config: db config relocated to src root

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+import config from 'config';
 
-const mongodbUrl = process.env.MONGODB_URL || 'mongodb://root:dev@localhost:27017/';
+const mongodbUrl = config.get('database.mongoUrl');
 const connect = () => mongoose.connect(mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
