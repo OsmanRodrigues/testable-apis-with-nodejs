02631514736317fc37ac5089934a9bb3ecd87c3f
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-08 11:28:21 -0300

    tdd(refactor): auth middleware refactored

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,10 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
 export default (req, res, next) => { 
-    next();
+    const token = req.headers['x-access-token'];
+    jwt.verify(token, config.get('auth.key'), (err, decoded) => {
+        req.decoded = decoded;
+        next(err);
+    });
 };
